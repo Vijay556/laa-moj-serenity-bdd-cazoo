58c6670ac5fb611bba2c0de69ed359ffa8405ccc
vijay556 <vijay.20886@gmail.com>
2023-09-14 20:27:12 +0100

    fix locator and import

--- src/test/java/cazoo/pages/MoreAboutYourCarPage.java
+++ src/test/java/cazoo/pages/MoreAboutYourCarPage.java
@@ -1,12 +1,12 @@
 package cazoo.pages;
 
-import net.serenitybdd.core.exceptions.NoSuchElementException;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.NoSuchElementException;
 import cazoo.Car;
 
 public class MoreAboutYourCarPage {
-    public static Target MAKE_MODEL = Target.the("Make and model details").locatedBy("//p[contains(text(),'Make/model: ')]");
+    public static Target MAKE_MODEL = Target.the("Make and model details").locatedBy("//p[contains(text(),'Make/model')]");
 
     public static Car getCarMakeModel(Car car, Actor actor) {
         String makeModel;
